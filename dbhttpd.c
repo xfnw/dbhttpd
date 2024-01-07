207481cf1acae6a52edb1875837511ecf141375d
xfnw <xfnw@riseup.net>
2024-01-07 17:37:51 -0500

    turn off http keep-alive
    keep-alive seems to be weirdly buggy on modern browsers, causes pages
    that exist to show up as 404, and going to different urls gives the old
    url's content...

--- dbhttpd.c
+++ dbhttpd.c
@@ -98,6 +98,9 @@ void handle_request(struct http_request_s *req) {
 	http_string_t method = http_request_method(req);
 	struct http_response_s *res = http_response_init();
 
+	/* turn off http keep-alive */
+	http_request_connection(req, HTTP_CLOSE);
+
 	/* be naughty and dont check the entire method name,
 	 * GET and PUT are the only 3-char methods starting
 	 * with G or P */
