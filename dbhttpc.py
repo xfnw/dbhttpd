cd89f2419bed72dcc1c86c16ab94cbe5024920e8
xfnw <xfnw@riseup.net>
2024-01-27 08:15:41 -0500

    dbhttpc: get/put works

--- dbhttpc.py
+++ dbhttpc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, errno
+import sys, errno, requests
 from stat import S_IFDIR, S_IFREG
 from fuse import FUSE, FuseOSError, Operations
 
@@ -8,14 +8,19 @@ from fuse import FUSE, FuseOSError, Operations
 class DbFS(Operations):
     def __init__(self, prefix):
         self.prefix = prefix
-        self.cache = {}
+        self.modified = set()
+        self.cache = dict()
 
     def _getfile(self, path):
         if path not in self.cache:
-            self.cache[path] = b"meow meow meow"
+            req = requests.get(self.prefix + path)
+            self.cache[path] = req.content
 
         return self.cache[path]
 
+    # TODO: handle making fake directories, since fuse does not let us
+    # have slashes in file names and dbhttpd does not have a concept
+    # of normal path rules
     def getattr(self, path, fh=None):
         if path == "/":
             m = S_IFDIR
@@ -35,6 +40,12 @@ class DbFS(Operations):
 
     def read(self, path, size, offset, fh):
         return self._getfile(path)[offset : offset + size]
+
+    def release(self, path, fh):
+        if path in self.modified:
+            requests.put(url=self.prefix + path, data=self.cache[path])
+            self.modified.remove(path)
+        del self.cache[path]
 
 
 if __name__ == "__main__":
