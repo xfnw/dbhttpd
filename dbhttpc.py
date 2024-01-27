67e4756fd426ce26d29001de9a438edf525bdb82
xfnw <xfnw@riseup.net>
2024-01-27 08:58:13 -0500

    dbhttpc: add writing support

--- dbhttpc.py
+++ dbhttpc.py
@@ -14,13 +14,18 @@ class DbFS(Operations):
     def _getfile(self, path):
         if path not in self.cache:
             req = requests.get(self.prefix + path)
-            self.cache[path] = req.content
+            if req.status_code == 404:
+                # dbhttpd does not have a distinction between
+                # empty and nonexistent files
+                self.cache[path] = b''
+            else:
+                self.cache[path] = req.content
 
         return self.cache[path]
 
-    # TODO: handle making fake directories, since fuse does not let us
-    # have slashes in file names and dbhttpd does not have a concept
-    # of normal path rules
+    # TODO: handle automatically making fake directories, since fuse
+    # does not let us have slashes in file names and dbhttpd does not
+    # have a concept of normal path rules
     def getattr(self, path, fh=None):
         if path == "/":
             m = S_IFDIR
@@ -38,14 +43,37 @@ class DbFS(Operations):
             st_size=size,
         )
 
+    def truncate(self, path, length, fh=None):
+        old = self._getfile(path)
+        self.cache[path] = old[:length].ljust(length, b'\0')
+
+
+    def unlink(self, path):
+        requests.put(url=self.prefix + path);
+
+        if path in self.modified:
+            self.modified.remove(path)
+        if path in self.cache:
+            del self.cache[path]
+
     def read(self, path, size, offset, fh):
         return self._getfile(path)[offset : offset + size]
+
+    def write(self, path, buf, offset, fh):
+        old = self._getfile(path)
+        self.cache[path] = old[:offset].ljust(offset, b'\0') + buf + old[offset + len(buf) :]
+        self.modified.add(path)
+
+        # what is this supposed to do?
+        # borrowed from example, errors out without it...
+        return len(self.cache)
 
     def release(self, path, fh):
         if path in self.modified:
             requests.put(url=self.prefix + path, data=self.cache[path])
             self.modified.remove(path)
-        del self.cache[path]
+        if path in self.cache:
+            del self.cache[path]
 
 
 if __name__ == "__main__":
